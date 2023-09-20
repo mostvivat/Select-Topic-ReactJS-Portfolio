8ec9d50bec2cea4eaa5af125be44d1e67b395dbf
Vivat Chongsomchit <pokemost@gmail.com>
2023-09-21 02:01:59 +0700

    Create s

---
+++ src/components/homepage/works.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+
+import Card from "../common/card";
+
+import "./styles/works.css";
+
+const Works = () => {
+	return (
+		<div className="works">
+			<Card
+				icon={faBriefcase}
+				title="Experiences"
+				body={
+					<div className="works-body">
+						<div className="work">
+							<img
+								src="./facebook.png"
+								alt="facebook"
+								className="work-image"
+							/>
+							<div className="work-title">UNDERGRADUATE</div>
+							<div className="work-subtitle">
+								KMUNTB
+							</div>
+							<div className="work-duration">2020 - Present</div>
+						</div>
+
+						<div className="work">
+							<img
+								src="./twitter.png"
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">HIGH SCHOOL</div>
+							<div className="work-subtitle">
+								HORWANG
+							</div>
+							<div className="work-duration">2017 - 2020</div>
+						</div>
+					</div>
+				}
+			/>
+		</div>
+	);
+};
+
+export default Works;
---
+++ src/data/user.js
@@ -0,0 +1,87 @@
+const INFO = {
+	main: {
+		title: "ReactPortfolio by Most",
+		name: "Vivat Chongsomchit",
+		email: "pokemost@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/mostvivat",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Software Tester /Quality Assurance",
+		description:
+			"hello",
+	},
+
+	about: {
+		title: "I’m Vivat Chongsomchit I live in Thailand.",
+		description:
+			"hi",
+	},
+
+
+	projects: [
+		{
+			title: "PHP WEBAPPLICATION WEBBOARD",
+			description:
+				"This is my first project. I Learn in university to make form website using HTML SQL & PHP.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/mostvivat/Webapplication-PHP-EnET-C.git",
+		},
+
+		{
+			title: "KEPTANG Web Application",
+			description:
+				"This is my first project that developed by RUST language.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/mostvivat/Project-Keptang.git",
+		},
+
+		{
+			title: "BOOTSTRAP PERSONAL WEBSITE",
+			description:
+				"This is my project for Webapplication subject.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/mostvivat/Webapplicaion-enet-c.git",
+		},
+
+		{
+			title: "LITTLE BUDDY Mobile Application",
+			description:
+				"This is my final project develop by using Flutter.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Yunocat02/LittleBuddy.git",
+		},
+
+		{
+			title: "BOOTSTRAP COMPUTER OFFICE WEBSITE",
+			description:
+				"This is my project for Webapplication subject.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/mostvivat/Webapplicaion-enet-c.git",
+		},
+		{
+			title: "PORTFOLIO Website",
+			description:
+				"This is my project for personal portfolio using HTML, CSS, JS & BOOTSTRAP.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/mostvivat/mostvivat.github.io.git",
+
+		}
+	],
+};
+
+export default INFO;
